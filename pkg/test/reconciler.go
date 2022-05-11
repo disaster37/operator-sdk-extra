45fa4b35696c8139ca18eb75d1e3b321e3bb4f14
disaster37 <linuxworkgroup@hotmail.com>
2022-05-11 09:46:04 +0000

    feat: inject object if exist on step
    Signed-off-by: disaster37 <linuxworkgroup@hotmail.com>

--- pkg/test/reconciler.go
+++ pkg/test/reconciler.go
@@ -1,9 +1,12 @@
 package test
 
 import (
-	"errors"
+	"context"
 	"testing"
+	"time"
 
+	k8serrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
@@ -12,20 +15,28 @@ type TestCase struct {
 	PreTest func(data map[string]any) error
 	Steps   []TestStep
 
+	key    types.NamespacedName
+	wait   time.Duration
+	object client.Object
 	data   map[string]any
 	t      *testing.T
 	client client.Client
 }
 
 type TestStep struct {
-	Do    func(c client.Client, data map[string]any) error
-	Check func(t *testing.T, c client.Client, data map[string]any) error
+	Do    func(c client.Client, o client.Object, data map[string]any) error
+	Check func(t *testing.T, c client.Client, o client.Object, data map[string]any) error
 }
 
-func NewTestCase(t *testing.T, c client.Client) *TestCase {
+func NewTestCase(t *testing.T, c client.Client, key types.NamespacedName, o client.Object, wait time.Duration) *TestCase {
 	return &TestCase{
 		t:      t,
 		client: c,
+		object: o,
+		wait:   wait,
+		key:    key,
+		data:   map[string]any{},
+		Steps:  make([]TestStep, 0),
 	}
 }
 
@@ -41,11 +52,17 @@ func (h *TestCase) Run() {
 	}
 
 	for _, step := range h.Steps {
-		if err = step.Do(h.client, h.data); err != nil {
+		if err = h.client.Get(context.Background(), h.key, h.object); err != nil {
+			if k8serrors.IsNotFound(err) {
+				h.object = nil
+			}
+		}
+		if err = step.Do(h.client, h.object, h.data); err != nil {
 			h.t.Fatal(err)
 		}
-		if err = step.Check(h.t, h.client, h.data); err != nil {
+		if err = step.Check(h.t, h.client, h.object, h.data); err != nil {
 			h.t.Fatal(err)
 		}
+		time.Sleep(h.wait)
 	}
 }
