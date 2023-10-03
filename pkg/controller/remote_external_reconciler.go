920c9fcab31e09d1a675a2fdc5c3f0dc04767359
disaster37 <linuxworkgroup@hotmail.com>
2023-10-03 07:48:35 +0000

    feat: Add basic diff for RemoteExternalReconciler

--- pkg/controller/remote_external_reconciler.go
+++ pkg/controller/remote_external_reconciler.go
@@ -1,7 +1,11 @@
 package controller
 
 import (
+	"reflect"
+
+	"emperror.dev/errors"
 	"github.com/disaster37/generic-objectmatcher/patch"
+	jsonIterator "github.com/json-iterator/go"
 )
 
 // RemoteExternalReconciler is the interface to call the remote API to handler resource
@@ -12,4 +16,27 @@ type RemoteExternalReconciler[k8sObject comparable, apiObject comparable] interf
 	Update(object apiObject) (err error)
 	Delete(name string) (err error)
 	Diff(currentOject apiObject, expectedObject apiObject, originalObject apiObject, ignoresDiff ...patch.CalculateOption) (patchResult *patch.PatchResult, err error)
+}
+
+// BasicRemoteExternalReconciler is the basic implementation of RemoteExternalReconciler
+// It only implement the Diff method, because of is generic with 3-way merge patch
+type BasicRemoteExternalReconciler[k8sObject comparable, apiObject comparable] struct{}
+
+func (h *BasicRemoteExternalReconciler[k8sObject, apiObject]) Diff(currentOject apiObject, expectedObject apiObject, originalObject apiObject, ignoresDiff ...patch.CalculateOption) (patchResult *patch.PatchResult, err error) {
+	if reflect.ValueOf(currentOject).IsNil() {
+		expected, err := jsonIterator.ConfigCompatibleWithStandardLibrary.Marshal(expectedObject)
+		if err != nil {
+			return nil, errors.Wrap(err, "Failed to convert expected object to byte sequence")
+		}
+
+		return &patch.PatchResult{
+			Patch:    expected,
+			Current:  expected,
+			Modified: expected,
+			Original: nil,
+			Patched:  expectedObject,
+		}, nil
+	}
+
+	return patch.DefaultPatchMaker.Calculate(currentOject, expectedObject, originalObject, ignoresDiff...)
 }
