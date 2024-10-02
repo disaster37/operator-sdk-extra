f053953a8fb320105701434f79e82c6656c7f738
disaster37 <linuxworkgroup@hotmail.com>
2024-10-02 13:56:21 +0000

    feat: Add webhook registers
    Signed-off-by: disaster37 <linuxworkgroup@hotmail.com>

--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -5,11 +5,15 @@ import (
 
 	"emperror.dev/errors"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 )
 
 // Indexer is a function to add indexer on manager
 type Indexer func(mgr ctrl.Manager) error
+
+// WebhookRegister is a function to add Webhook on manager
+type WebhookRegister func(mgr ctrl.Manager, client client.Client) error
 
 // Controller is the controller interface
 type Controller interface {
@@ -40,6 +44,17 @@ func (h *BasicController) Reconcile(context.Context, reconcile.Request) (res rec
 func SetupIndexerWithManager(mgr ctrl.Manager, indexers ...Indexer) (err error) {
 	for _, indexer := range indexers {
 		if err = indexer(mgr); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// SetupWebhookWithManager permit to registers webhooks on manager
+func SetupWebhookWithManager(mgr ctrl.Manager, client client.Client, webhookRegisters ...WebhookRegister) (err error) {
+	for _, webhookRegister := range webhookRegisters {
+		if err = webhookRegister(mgr, client); err != nil {
 			return err
 		}
 	}
