db6c79ee135316c1f319390d21d4772a28ad26de
disaster37 <linuxworkgroup@hotmail.com>
2025-01-23 13:51:32 +0000

    feat: add github ci
    Signed-off-by: disaster37 <linuxworkgroup@hotmail.com>

--- ci/dagger/main.go
+++ ci/dagger/main.go
@@ -17,18 +17,21 @@ package main
 import (
 	"context"
 	"dagger/operator-sdk-extra/internal/dagger"
+
+	"emperror.dev/errors"
+	"github.com/disaster37/dagger-library-go/lib/helper"
 )
 
 const (
-	kubeVersion                 = "1.31.0"
-	sdkVersion                  = "v1.37.0"
-	controllerGenVersion        = "v0.16.1"
-	kustomizeVersion            = "v5.4.3"
-	cleanCrdVersion             = "v0.1.9"
-	opmVersion                  = "v1.48.0"
-	gitUsername          string = "github"
-	gitEmail             string = "github@localhost"
-	defaultBranch               = "main"
+	kubeVersion          = "1.31.0"
+	sdkVersion           = "v1.37.0"
+	controllerGenVersion = "v0.16.1"
+	kustomizeVersion     = "v5.4.3"
+	cleanCrdVersion      = "v0.1.9"
+	opmVersion           = "v1.48.0"
+	gitUsername          = "github"
+	gitEmail             = "github@localhost"
+	defaultBranch        = "main"
 )
 
 type OperatorSdkExtra struct {
@@ -36,7 +39,11 @@ type OperatorSdkExtra struct {
 	Src *dagger.Directory
 
 	// +private
-	*dagger.OperatorSDK
+	OperatorSDK *dagger.OperatorSDK
+
+	// +private
+	Golang *dagger.Golang
+
 }
 
 func New(
@@ -44,9 +51,13 @@ func New(
 	// +required
 	src *dagger.Directory,
 ) *OperatorSdkExtra {
+
+	cleanDir := src.WithoutDirectory("ci").WithoutDirectory("samples")
 	return &OperatorSdkExtra{
 		Src:         src,
-		OperatorSDK: dag.OperatorSDK(src.WithoutDirectory("ci").WithoutDirectory("samples")),
+		OperatorSDK: dag.OperatorSDK(cleanDir),
+		Golang:      dag.Golang(cleanDir),
+
 	}
 }
 
@@ -71,7 +82,7 @@ func (h *OperatorSdkExtra) Test(
 	// +optional
 	path string,
 ) *dagger.File {
-	return h.Golang().Test(dagger.OperatorSDKGolangTestOpts{
+	return h.OperatorSDK.Golang().Test(dagger.OperatorSDKGolangTestOpts{
 		Short:           short,
 		Shuffle:         shuffle,
 		Run:             run,
@@ -80,4 +91,129 @@ func (h *OperatorSdkExtra) Test(
 		Path:            path,
 		WithKubeversion: kubeVersion,
 	})
+}
+
+// Release permit to release to operator version
+func (h *OperatorSdkExtra) CI(
+	ctx context.Context,
+
+	// Set true to run tests
+	// +optional
+	ci bool,
+
+	// Set true if current build is a tag
+	// It will use the stable and alpha channel
+	// alpha channel only instead
+	// +optional
+	isTag bool,
+
+	// Set true if current build is a Pull request
+	// +optional
+	isPullRequest bool,
+
+	// Set the current branch name. It's needed because of CI overwrite the branch name by PR
+	// +optional
+	branchName string,
+
+	// Set true to skip test
+	// +optional
+	skipTest bool,
+
+	// The git token
+	// +optional
+	gitToken *dagger.Secret,
+
+	// The codeCov token
+	// +optional
+	codeCoveToken *dagger.Secret,
+) (*dagger.Directory, error) {
+	var dir *dagger.Directory
+	var err error
+
+	// Generate manifests
+	dir = h.OperatorSDK.SDK().GenerateManifests()
+	h.OperatorSDK = h.OperatorSDK.WithSource(dir)
+	h.Golang = h.Golang.WithSource(dir)
+
+	// Format code
+	dir = h.Golang.Format()
+	h.OperatorSDK = h.OperatorSDK.WithSource(dir)
+	h.Golang = h.Golang.WithSource(dir)
+
+	// Lint code
+	if _, err = h.Golang.Lint(ctx); err != nil {
+		return nil, errors.Wrap(err, "Error when lint code")
+	}
+
+	// Vuln check
+	if _, err = h.Golang.Vulncheck(ctx); err != nil {
+		return nil, errors.Wrap(err, "Error when check vulnerability")
+	}
+
+	// Test code
+	if !skipTest {
+		coverageFile := h.Test(
+			ctx,
+			false,
+			false,
+			"",
+			"",
+			true,
+			"",
+		)
+		dir = dir.WithFile("coverage.out", coverageFile)
+	}
+
+	if ci {
+
+		// Put ci folder to not lost it
+
+		// codecov
+		if _, err := dag.Codecov().Upload(
+			ctx,
+			dir,
+			codeCoveToken,
+			dagger.CodecovUploadOpts{
+				Files:   []string{"coverage.out"},
+				Verbose: true,
+				InjectCiEnvironment: true,
+			},
+		); err != nil {
+			return nil, errors.Wrap(err, "Error when upload report on CodeCov")
+		}
+
+		// Commit / push
+		var branch string
+		git := dag.Git().
+			SetConfig(gitUsername, gitEmail, dagger.GitSetConfigOpts{BaseRepoURL: "github.com", Token: gitToken})
+
+		if !isTag {
+			if branchName == "" {
+				return nil, errors.New("You need to provide the branch name")
+			}
+			branch = branchName
+		} else {
+			branch = defaultBranch
+		}
+
+		if isPullRequest {
+			git = git.With(func(r *dagger.Git) *dagger.Git {
+				ctr := r.BaseContainer().
+					WithDirectory("/project", dir).
+					WithWorkdir("/project").
+					WithExec(helper.ForgeCommand("git remote -v")).
+					WithExec(helper.ForgeCommandf("git fetch origin %s:%s", branch, branch)).
+					WithExec(helper.ForgeCommandf("git checkout %s", branch))
+
+				return r.WithCustomContainer(ctr)
+			})
+		} else {
+			git = git.SetRepo(h.Src.WithDirectory(".", dir), dagger.GitSetRepoOpts{Branch: branch})
+		}
+		if _, err = git.CommitAndPush(ctx, "Commit from CI pipeline"); err != nil {
+			return nil, errors.Wrap(err, "Error when commit and push files change")
+		}
+	}
+
+	return dir, nil
 }
