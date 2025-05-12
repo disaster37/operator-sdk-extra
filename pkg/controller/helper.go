58077f4096999c37ec8b11e777c7e9159af48357
disaster37 <linuxworkgroup@hotmail.com>
2025-05-12 09:37:30 +0000

    feat: Add helper to ensure Networkpolicy for Webhook.

--- pkg/controller/helper.go
+++ pkg/controller/helper.go
@@ -1,13 +1,23 @@
 package controller
 
 import (
+	"context"
 	"reflect"
 	"time"
 
+	"emperror.dev/errors"
+	"github.com/disaster37/k8s-objectmatcher/patch"
 	"github.com/disaster37/operator-sdk-extra/v2/pkg/apis/shared"
+	"github.com/sirupsen/logrus"
 	"golang.org/x/time/rate"
+	v1 "k8s.io/api/core/v1"
+	networkv1 "k8s.io/api/networking/v1"
+	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/util/intstr"
 	"k8s.io/client-go/util/workqueue"
+	"k8s.io/utils/ptr"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
@@ -80,4 +90,67 @@ func DefaultControllerRateLimiter[T comparable]() workqueue.TypedRateLimiter[T] 
 		workqueue.NewTypedItemExponentialFailureRateLimiter[T](1*time.Second, 1000*time.Second),
 		&workqueue.TypedBucketRateLimiter[T]{Limiter: rate.NewLimiter(rate.Limit(10), 100)},
 	)
+}
+
+// EnsureNetworkPolicyForWebhook permit to create / update NetworkPolicy for webhook
+func EnsureNetworkPolicyForWebhook(c client.Client, logger *logrus.Entry, namespace string, labels map[string]string, podSelecetors map[string]string) error {
+	networkPolicy := &networkv1.NetworkPolicy{}
+	expectedNetworkPolicy := &networkv1.NetworkPolicy{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: namespace,
+			Name:      "allow-webhook-access-from-any",
+			Labels:    labels,
+		},
+		Spec: networkv1.NetworkPolicySpec{
+			PodSelector: metav1.LabelSelector{
+				MatchLabels: podSelecetors,
+			},
+			Ingress: []networkv1.NetworkPolicyIngressRule{
+				{
+					From: []networkv1.NetworkPolicyPeer{},
+					Ports: []networkv1.NetworkPolicyPort{
+						{
+							Protocol: ptr.To(v1.ProtocolTCP),
+							Port:     ptr.To(intstr.FromInt(9443)),
+						},
+					},
+				},
+			},
+		},
+	}
+
+	if err := c.Get(context.Background(), types.NamespacedName{Namespace: expectedNetworkPolicy.GetNamespace(), Name: expectedNetworkPolicy.GetName()}, networkPolicy); err != nil {
+		// Create
+		if k8serrors.IsNotFound(err) {
+			// Set diff 3-way annotations
+			if err := patch.DefaultAnnotator.SetLastAppliedAnnotation(networkPolicy); err != nil {
+				return errors.Wrap(err, "Error when set annotation for 3-way diff on NetworkPolicy for webhook")
+			}
+			if err = c.Create(context.Background(), expectedNetworkPolicy); err != nil {
+				return errors.Wrap(err, "Error when create NetworkPolicy for webhook")
+			}
+
+			logger.Info("Successfully create networkPolicy for webhook")
+			return nil
+		}
+		return errors.Wrap(err, "Error when get NetworkPolicy for webhook")
+	}
+
+	// Diff
+	MustInjectTypeMeta(networkPolicy, expectedNetworkPolicy)
+	patchResult, err := patch.DefaultPatchMaker.Calculate(networkPolicy, expectedNetworkPolicy)
+	if err != nil {
+		return errors.Wrap(err, "Error when diffing NetworkPolicy for webhook")
+	}
+
+	// Update
+	if !patchResult.IsEmpty() {
+		networkPolicy = patchResult.Patched.(*networkv1.NetworkPolicy)
+		if err = c.Update(context.Background(), networkPolicy); err != nil {
+			return errors.Wrap(err, "Error when update NetworkPolicy for webhook")
+		}
+		logger.Info("Successfully update networkPolicy for webhook")
+	}
+
+	return nil
 }
